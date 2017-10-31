e6fb416fcaead000c4e24e6b7a16ef19bcacd0b0
kylepeng123 <kylepeng123@gmail.com>
2017-10-31 15:50:32 -0400

    exec_testing_second

--- using_exec.cpp
+++ using_exec.cpp
@@ -30,23 +30,19 @@ int procC(void) {
     return 0;
 }
 int main(void) {
- int AtoB[2];
+	std::vector<pid_t> kids;
+	int AtoB[2];
     pipe(AtoB);
     int AtoC[2];
     pipe(AtoC);
-    char* argv[3];
-
-    argv[0] = (char*)"python";
-    argv[1] = (char*)"a1ece650.py";
-    argv[2] = nullptr;
-    std::cout << "[exec] executing '/bin/ls -l' using execv" << std::endl;
+    
 
     pid_t kid;
     kid = fork ();
 	if (kid == 0) {
-	  dup2(AtoC[1],STDOUT_FILENO);
-    close(AtoC[0]);
-    close(AtoC[1]);
+		dup2(AtoC[1],STDOUT_FILENO);
+		close(AtoC[0]);
+		close(AtoC[1]);
  
         //sleep (4);
         execlp ("./using_getopt", "rgen", (char*) NULL);
@@ -54,41 +50,61 @@ int main(void) {
         perror ("Error from arie");
         return 1;
     }
+	else if (kid < 0) {
+        std::cerr << "Error: could not fork\n";
+        return 1;
+    }
+	kids.push_back(kid);
+	kid = fork ();
     if (kid == 0) {
-	dup2(AtoC[0],STDIN_FILENO);
-	close(AtoC[1]);
-	close(AtoC[0]);
-dup2(AtoB[1],STDOUT_FILENO);
-	close(AtoB[0]);
-	close(AtoB[1]);
+		dup2(AtoC[0],STDIN_FILENO);
+		close(AtoC[1]);
+		close(AtoC[0]);
+	
+		dup2(AtoB[1],STDOUT_FILENO);
+		close(AtoB[0]);
+		close(AtoB[1]);
         //sleep (4);
         execlp ("python", "python", "a1ece650.py", "test", (char*) NULL);
         // execl("/bin/ls", "ls", "-l", nullptr);
         perror ("Error from arie");
         return 1;
+    }else if (kid < 0) {
+        std::cerr << "Error: could not fork\n";
+        return 1;
     }
   
-
+	kids.push_back(kid);
 	std::cout <<"B start:"<<  std::endl;
  	
     kid = fork ();
-   if (kid == 0) {
-	dup2(AtoB[0],STDIN_FILENO);
-	close(AtoB[1]);
-	close(AtoB[0]);
+    if (kid == 0) {
+		dup2(AtoB[0],STDIN_FILENO);
+		close(AtoB[1]);
+		close(AtoB[0]);
         //sleep (4);
         
-        // execl("/bin/ls", "ls", "-l", nullptr);
+        execlp("./ece650-a2", "a2", nullptr);
         
         return procC();
     }
-   dup2(AtoB[1],STDOUT_FILENO);
+	else if (kid < 0) {
+        std::cerr << "Error: could not fork\n";
+        return 1;
+    }
+	kids.push_back(kid);
+	child_pid = 0;
+	dup2(AtoB[1],STDOUT_FILENO);
     close(AtoB[0]);
     close(AtoB[1]);
     // start process B
     int res =  procB();
-   std::cout <<"B end:"<<  std::endl;
-    waitpid(kid, &res, 0);
+    std::cout <<"B end:"<<  std::endl;
+	for (pid_t k : kids) {
+        int status;
+        kill (k, SIGTERM);
+        waitpid(k, &status, 0);
+    }
     std::cout << "ls returned status: " << res << std::endl;
 
     return 0;
